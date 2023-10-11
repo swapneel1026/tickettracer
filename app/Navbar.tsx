ca9b3b852fd4f594944bb5916b063af447cd8d17
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-12 02:48:27 +0530

    feat:build navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,18 +1,36 @@
-import Link from "next/link"
-import { FaBug } from 'react-icons/fa6'
+import Link from "next/link";
+import { FaBug } from "react-icons/fa6";
 
 const Navbar = () => {
+  interface links {
+    name: string;
+    href: string;
+  }
+  const links: links[] = [
+    { name: "Dashboard", href: "/dashboard" },
+    { name: "Tickets", href: "/tickets" },
+  ];
   return (
-    <nav>
-        <Link href={"/"}>
-        <FaBug/>
-        </Link>
-        <ul>
-            <li></li>
-            <li></li>
-        </ul>
+    <nav className="p-4 flex space-x-4 border-b-2">
+      <Link href={"/"} className="flex space-x-1 items-center">
+        <FaBug />
+        <p className="font-extrabold">TicketTracer</p>
+      </Link>
+      <ul className="flex space-x-2 items-center">
+        {links.map((link) => {
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              className="text-sm font-semibold cursor-pointer text-zinc-400 hover:text-zinc-700 hover:transition-colors hover:duration-300"
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
