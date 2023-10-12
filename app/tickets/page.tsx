1de7c6b1ceb5f95c2093d7eb107ba9e2c8abb454
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-12 18:45:01 +0530

    feat:add ticket button navigator

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+import { Button } from "@radix-ui/themes"
+import { useRouter } from "next/navigation"
 
 const TicketsPage = () => {
+  const router=useRouter()
   return (
-    <div>TicketsPage</div>
+    <div className="p-6">
+      <Button onClick={()=>{router.push("/tickets/new")}}>Add new Ticket</Button>
+    </div>
   )
 }
 
