8215bb04a671c7d34da4adf345543c2c4c112b59
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-12 21:29:08 +0530

    fix:max input value in api

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -2,8 +2,8 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 const ticketSchema = z.object({
-  title: z.string().min(1,"Description is Required").max(999),
-  description: z.string().min(1,"Title is required").max(255),
+  title: z.string().min(1,"Title is required").max(255),
+  description: z.string().min(1,"Description is Required"),
 });
 
 export async function POST(request: NextRequest) {
