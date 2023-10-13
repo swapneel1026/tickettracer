4bd43490a846f9046632c3a59bf041084c6ca80a
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-13 12:37:40 +0530

    feat:display all issues in table

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,18 +1,39 @@
-"use client";
-import { Button } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import prisma from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
 
-const TicketsPage = () => {
-  const router = useRouter();
+const TicketsPage = async() => {
+  const allTickets=await prisma.ticket.findMany()
+  
   return (
     <div className="p-4">
-      <Button
-        onClick={() => {
-          router.push("/tickets/new");
-        }}
-      >
+      <Button>
+        <Link href={"/tickets/new"}>
         Add new Ticket
+        </Link>
       </Button>
+      <Table.Root>
+        <Table.Header>
+        <Table.Row >
+            <Table.Cell>Ticket Title</Table.Cell>
+            <Table.Cell>Issue</Table.Cell>
+            <Table.Cell>Created</Table.Cell>
+          </Table.Row>
+          </Table.Header>
+          <Table.Body>
+
+          {allTickets?.map((ticket)=>{
+            return(
+
+          <Table.Row key={ticket.id}>
+            <Table.Cell>{ticket.title}</Table.Cell>
+            <Table.Cell className="max-w-[15rem] truncate">{ticket.description}</Table.Cell>
+            <Table.Cell>{ticket.createdAt.toDateString()}</Table.Cell>
+          </Table.Row>
+            )
+          })}
+          </Table.Body>
+      </Table.Root>
     </div>
   );
 };
