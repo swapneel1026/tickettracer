e4c500341c7286e08c88bea38ddd6616e5855fa5
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-14 12:07:13 +0530

    fix:table orientation in mobile view

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,44 +1,53 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import delay from 'delay';
 import StatusBagde from "../components/StatusBagde";
 import TicketActionBar from "../components/ticketAction";
 
-const TicketsPage = async() => {
-  const allTickets=await prisma.ticket.findMany()
-  await delay(2000)
-  
+const TicketsPage = async () => {
+  const allTickets = await prisma.ticket.findMany();
+  // await delay(2000)
+
   return (
     <div className="px-6 py-4">
-     <TicketActionBar />
-      <Table.Root variant="surface" className="mt-4" >
+      <TicketActionBar />
+      <Table.Root variant="surface" className="mt-4">
         <Table.Header>
-        <Table.Row>
+          <Table.Row>
             <Table.ColumnHeaderCell>Ticket Title</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Created At</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created At
+            </Table.ColumnHeaderCell>
           </Table.Row>
-          </Table.Header>
-          <Table.Body>
-
-          {allTickets?.map((ticket)=>{
-            return(
-
-          <Table.Row key={ticket.id}>
-            <Table.Cell className="capitalize">{ticket.title}
-            <small className="block md:hidden">{ticket.status}</small>
-            </Table.Cell>
-            <Table.Cell className="hidden md:table-cell">
-              <StatusBagde status={ticket.status}/>
-            </Table.Cell>
-            <Table.Cell className="hidden md:table-cell ">{ticket.createdAt.toDateString()}</Table.Cell>
-          </Table.Row>
-            )
+        </Table.Header>
+        <Table.Body>
+          {allTickets?.map((ticket) => {
+            return (
+              <Table.Row key={ticket.id}>
+                <Table.Cell className="capitalize">
+                  {ticket.title}
+                  <span className="block md:hidden">
+                    <StatusBagde status={ticket.status} />
+                  </span>
+                  <span className="block md:hidden">
+                  {ticket.createdAt.toDateString()}
+                  </span>
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  <StatusBagde status={ticket.status} />
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell ">
+                  {ticket.createdAt.toDateString()}
+                </Table.Cell>
+              </Table.Row>
+            );
           })}
-          </Table.Body>
+        </Table.Body>
       </Table.Root>
     </div>
   );
 };
-export const revalidate='force';
+export const revalidate = "force";
 export default TicketsPage;
