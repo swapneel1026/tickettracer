21962d18866b6c6ed0e3a081e1f2b4af9e358cbc
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-14 13:28:47 +0530

    fix:form skeleton styling

--- app/tickets/new/loading.tsx
+++ app/tickets/new/loading.tsx
@@ -5,7 +5,7 @@ import Skeleton from "react-loading-skeleton";
 const loading = () => {
   return (
     <div className="max-w-xl p-4 flex flex-col space-y-3">
-   <Skeleton count={10} height={20} />
+   <Skeleton count={20} height={30} />
 
       
 
