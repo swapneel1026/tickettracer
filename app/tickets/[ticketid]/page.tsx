c26436e56b58eee2f6444cbf0b75f501ef7cf837
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-14 15:36:12 +0530

    fix:validation in params

--- app/tickets/[ticketid]/page.tsx
+++ app/tickets/[ticketid]/page.tsx
@@ -1,19 +1,24 @@
+import StatusBagde from "@/app/components/StatusBagde";
 import prisma from "@/prisma/client";
+import { Heading } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
 
-type Props = {
-  ticketid: string;
-};
-const TicketdescriptionPage = async ({ params }: { params: Props }) => {
+interface Props {
+  params: { ticketid: string };
+}
+
+const TicketdescriptionPage = async ({ params }: Props) => {
+  if (typeof params.ticketid !== "string") notFound();
   const ticket = await prisma.ticket.findUnique({
     where: { id: parseInt(params.ticketid) },
   });
+  if (!ticket) notFound();
 
   return (
     <div>
-      <p>{ticket?.id}</p>
-      <p>{ticket?.title}</p>
+      <Heading className="capitalize">{ticket?.title}</Heading>
+      <StatusBagde status={ticket.status} />
       <p>{ticket?.description}</p>
-      <p>{ticket?.status}</p>
       <p>{ticket?.updatedAt.toDateString()}</p>
     </div>
   );
