8121b9032f16c3929e649956b2dce26733fcd7d8
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-15 11:24:29 +0530

    feat:added avatar hovercard

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,17 @@
 "use client";
-import { Avatar, Button } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HoverCard,
+  Text,
+} from "@radix-ui/themes";
 import classnames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { IconContext } from "react-icons";
 import { FaBug } from "react-icons/fa6";
 
 const Navbar = () => {
@@ -20,8 +29,10 @@ const Navbar = () => {
     <nav className="py-4 px-6 mb-10 flex space-x-4 border-b-2 justify-between">
       <section className="flex space-x-8 items-center">
         <Link href={"/"} className="flex space-x-2 items-center">
-          <FaBug />
-          <p className="font-extrabold">TicketTracer</p>
+          <IconContext.Provider value={{ className: "text-blue-500" }}>
+            <FaBug />
+          </IconContext.Provider>
+          <p className="font-extrabold text-2xl text-blue-400">TicketTracer</p>
         </Link>
         <ul className="flex space-x-2 items-center">
           {links.map((link) => {
@@ -43,12 +54,38 @@ const Navbar = () => {
         </ul>
       </section>
       <ul className="flex space-x-3 items-center">
-        <Avatar
-          // src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-          fallback="ss"
-          radius="full"
-          variant="solid"
-        />
+        <HoverCard.Root>
+          <HoverCard.Trigger>
+            <Avatar
+              src="https://avatars.githubusercontent.com/u/62759701?v=4"
+              fallback="ss"
+              radius="full"
+              variant="solid"
+            />
+          </HoverCard.Trigger>
+          <HoverCard.Content>
+            <Flex gap="4">
+              <Avatar
+                src="https://avatars.githubusercontent.com/u/62759701?v=4"
+                size="5"
+                fallback="ss"
+                radius="full"
+              />
+              <Box>
+                <Heading size="3" as="h3">
+                  Swapneel Shubham
+                </Heading>
+                <Text as="div" size="2" color="gray">
+                  @swapneel1026
+                </Text>
+
+                <Text as="div" size="2" style={{ maxWidth: 300 }} mt="3">
+                  I love Next-JS
+                </Text>
+              </Box>
+            </Flex>
+          </HoverCard.Content>
+        </HoverCard.Root>
         <Button variant="soft">
           <Link href={"/login"}>Login</Link>
         </Button>
