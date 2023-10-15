777ddce2acf98829971b6cad54fa4bb2ab0215a3
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-15 13:48:29 +0530

    feat:delete implementation with confim box

--- app/tickets/[ticketid]/edit/_components/DeleteButton.tsx
+++ app/tickets/[ticketid]/edit/_components/DeleteButton.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
@@ -15,24 +14,49 @@ const DeleteButton = ({ id }: ticketProps) => {
   async function ticketdelete(id: any) {
     const deleteResponse = await axios.delete(`/api/tickets/${id}`);
     console.log(deleteResponse);
-    
-    if (deleteResponse.status===200) {
-      toast.success(`Ticket with Title-${deleteResponse?.data?.title} successfully deleted`, {
-        theme: "light",
-      });
-      router.push("/tickets");
 
+    if (deleteResponse.status === 200) {
+      toast.success(
+        `Ticket with successfully deleted`,
+        {
+          theme: "light",
+        }
+      );
+      router.push("/tickets");
     }
   }
   return (
-    <Button
-      style={{ marginTop: "16px" }}
-      color="red"
-      onClick={() => ticketdelete(id)}
-    >
-      <TrashIcon />
-      Delete Issues
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" style={{ marginTop: "16px" }}>
+          Delete Ticket
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Revoke access</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This ticket will no longer be accessible and you will be
+          redirected to all tickets page.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              variant="solid"
+              color="red"
+              onClick={() => ticketdelete(id)}
+            >
+              Delete Ticket
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
