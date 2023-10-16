baa09f5772d943298826b8f320d67fe33b956d78
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-16 22:38:56 +0530

    feat:added loggedin user table

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,20 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+
+const prisma=new PrismaClient()
 const authHandler= NextAuth({
+  adapter:PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
           clientId: process.env.GOOGLE_CLIENT_ID!,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-      ]
+      ],
+      session:{
+        strategy:'jwt'
+      }
 })
 
 export { authHandler as GET, authHandler as POST }
