d2a930b326bca36e71805b32967e8ad8d2b24c11
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-19 00:47:51 +0530

    fix:change relation mode prisma

--- prisma/migrations/20231013074943_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [OPENIN_PROGRESS] on the enum `Ticket_status` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `ticket` MODIFY `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN';
