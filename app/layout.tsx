47a46df426dae340b9446923551c5201b65479aa
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-20 03:23:16 +0530

    feat:dashboard creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,3 +39,4 @@ export default function RootLayout({
     </html>
   );
 }
+export const dynamic = 'force-dynamic'; 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,28 @@
+import prisma from "@/prisma/client";
+import { Box, Flex } from "@radix-ui/themes";
+import StatusCountBox from "./components/dashboard/StatusCountBox";
 
-export default function Home() {
+export default async function Home() {
+  const openTicketCount = await prisma.ticket.count({ where: { status: "OPEN" } });
+  const closedTicketCount = await  prisma.ticket.count({
+    where: { status: "CLOSED" },
+  });
+  const progressTicketCount =await prisma.ticket.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
+  console.log(openTicketCount, closedTicketCount, progressTicketCount);
+
   return (
-    <main className="text-xl text-blue-600">Dashboard</main>
-  )
+    <main className="">
+      <Box>
+        <Flex align="center" gap="4">
+          <StatusCountBox ticketTitle="Open" numberofTickets={openTicketCount} />
+          <StatusCountBox numberofTickets={closedTicketCount} ticketTitle="Closed" />
+          <StatusCountBox ticketTitle="In-Progress" numberofTickets={progressTicketCount} />
+        </Flex>
+      </Box>
+    </main>
+  );
 }
+export const dynamic = 'force-dynamic'; 
