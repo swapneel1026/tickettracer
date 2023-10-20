54ac496863aef6006af97d63e6462cd78e11fef6
Swapneel Shubham <62759701+swapneel1026@users.noreply.github.com>
2023-10-20 19:52:22 +0530

    Update SummaryTable.tsx

--- app/components/dashboard/SummaryTable.tsx
+++ app/components/dashboard/SummaryTable.tsx
@@ -13,7 +13,7 @@ const SummaryTable = async () => {
   });
   return (
     <div className="py-4">
-      <h1 className="p-4 font-extrabold md:text-xl text-blue-400">Latest Tickets</h1>
+      <h1 className="p-4 font-extrabold md:text-xl text-blue-400">Latest Tickets Updates</h1>
 
       <Table.Root variant="surface" className="mt-4">
         <Table.Header>
