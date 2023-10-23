735bf78bb0d62d34c5597b02456b8f162fe1c24b
swapneel1026 <swapneelshubham44@gmail.com>
2023-10-23 13:21:21 +0530

    feat:optional classname prop

--- app/components/ToolTip.tsx
+++ app/components/ToolTip.tsx
@@ -2,9 +2,9 @@
 import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 import { Tooltip } from "@radix-ui/themes";
 
-const ToolTip = ({toolTipText}:{toolTipText:string}) => {
+const ToolTip = ({toolTipText,className}:{toolTipText:string,className?:string}) => {
   return (
-    <Tooltip content={toolTipText} className="capitalize" style={{backgroundColor:"#60a5fa",border:"1px solid #60a5ff",fontWeight:"bold"}}>
+    <Tooltip content={toolTipText} className={`${className} capitalize`} style={{backgroundColor:"#60a5fa",border:"1px solid #60a5ff",fontWeight:"bold"}}>
         <QuestionMarkCircledIcon/>
     </Tooltip>
   );
